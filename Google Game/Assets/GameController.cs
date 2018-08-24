605416559736efb90b9f073ed0958ff8a84c2e52
michaelbridgette <c00205948@itcarlow.ie>
2018-08-24 16:35:49 +0100

    Game Finished

---
+++ Google Game/Assets/GameController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OpenStore()
+    {
+        Application.OpenURL("https://store.google.com/");
+    }
+    public void OpenHopper()
+    {
+        Application.OpenURL("https://play.google.com/store/apps/details?id=com.area120.grasshopper&hl=en");
+    }
+    public void OpenNews()
+    {
+        Application.OpenURL("https://news.google.com/news");
+    }
+}
